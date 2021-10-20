541c10ca04820d55da4dc03003c6d4066fe5db9a
nik2477 <sidnik2477@gmail.com>
2021-10-20 15:14:35 +0300

    Initial commit

---
+++ src/ns_n1.java
@@ -0,0 +1,7 @@
+public class ns_n1 {
+    public static void main(String[] args) {
+        int a = 13;
+        if (a%2 == 0) System.out.println(" a " + "четное число");
+        else System.out.println(" a " + "нечетное число");
+    }
+}
